ab230264976219224dcd8d95cb946070e31f0a2f
Amey Satwe <amey10051999@gmail.com>
2024-02-07 17:43:47 -0500

    Working on PUT request. Added multiple validations for fields in PUT request. Validations hit before the code goes to sequelize

--- main.js
+++ main.js
@@ -2,10 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const app = express();
-const port = 3000;
+const { body, validationResult } = require("express-validator");
 
 const sequelize = require("./models/sequelize.js");
 const User = require("./models/userModel.js");
+
+const putSchema = [
+  body("username").isEmail(),
+  body("first_name").isString(),
+  body("last_name").isString(),
+  body("password").isAlphanumeric(),
+];
+
+const validateSchema = (req, res, next) => {
+  const errors = validationResult(req);
+  const allowedFields = ["username", "first_name", "last_name", "password"];
+  const requestFields = Object.keys(req.body);
+  const extraFields = requestFields.filter(
+    (field) => !allowedFields.includes(field)
+  );
+
+  if (extraFields.length > 0) {
+    return res.status(400).json({ errors: "Invalid fields in put request" });
+  }
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 app.use(express.json());
 
@@ -82,8 +107,6 @@ app.post("/v1/user", async (req, res, next) => {
       password: hashedPwd,
       username: userName,
     });
-    // console.log(`${user.first_name}'s auto-generated ID:, ${user.id}`);
-
     return res.status(201).send();
   } catch (e) {
     console.log(e);
@@ -91,13 +114,19 @@ app.post("/v1/user", async (req, res, next) => {
   }
 });
 
-app.get("/v1/user/self", async (req, res, next) => {
-  // console.log(atob(req.headers.authorization.split(" ")[1]));
+const authMiddleWare = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    // let err = new Error("You are not authenticated!");
-    res.status(401).send();
+    return res.status(401).send();
+  } else {
+    return next();
   }
+};
+
+app.use("/v1/user/self", authMiddleWare);
+
+app.get("/v1/user/self", async (req, res, next) => {
+  const authHeader = req.headers.authorization;
   const decodedString = atob(authHeader.split(" ")[1]).split(":");
   const username = decodedString[0];
   const password = decodedString[1];
@@ -124,11 +153,43 @@ app.get("/v1/user/self", async (req, res, next) => {
   res.status(401).send();
 });
 
-app.put("/v1/user/self", async (req, res, next) => {
+app.put("/v1/user/self", putSchema, validateSchema, async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    // let err = new Error("You are not authenticated!");
-    res.status(401).send();
+  const decodedString = atob(authHeader.split(" ")[1]).split(":");
+  const username = decodedString[0];
+  const password = decodedString[1];
+  const currentUser = await User.findOne({
+    where: {
+      username: username,
+    },
+  });
+  if (!currentUser) {
+    return res.status(400).send();
+  }
+  const isValidUser = await bcrypt.compare(password, currentUser.password);
+  if (currentUser && !isValidUser) {
+    // password validation has failed
+    return res.status(401).send();
+  }
+  let body = req.body;
+  let firstName = body.first_name;
+  let lastName = body.last_name;
+  let updatedPassword = body.password;
+  const hashedPwd = await bcrypt.hash(updatedPassword, 10);
+  console.log("here");
+  try {
+    currentUser.set({
+      first_name: firstName,
+      last_name: lastName,
+      password: hashedPwd,
+    });
+    console.log("After set");
+
+    await currentUser.save();
+    return res.status(204).send();
+  } catch (err) {
+    console.log(err);
+    res.status(400).send();
   }
 });
 
@@ -141,6 +202,9 @@ sequelize
     console.error("Error syncing database:", error);
   });
 
+app.use("/", (req, res, next) => {
+  return res.status(404).send();
+});
 // app.listen(3000, () => console.log(`Listening on port: 3000`));
 
 module.exports = app;
