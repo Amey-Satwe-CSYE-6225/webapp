26b36191be219558879982ec1427e2ec10a2b5f0
Amey Satwe <amey10051999@gmail.com>
2024-02-07 20:22:32 -0500

    Update some console.logs. Add some response error messages

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const validateSchema = (req, res, next) => {
   }
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: "Please check fields" });
   }
   next();
 };
