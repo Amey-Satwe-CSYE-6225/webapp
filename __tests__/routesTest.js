d8bc01cb2a00f9daaeabb0e6ddbb3a2c5394de80
Amey Satwe <amey10051999@gmail.com>
2024-02-11 13:11:42 -0500

    change error code from failing to passing

--- __tests__/routesTest.js
+++ __tests__/routesTest.js
@@ -12,7 +12,7 @@ const payload = {
 describe("GET /healthz", () => {
   it("responds with 200 if db connects", async () => {
     const response = await request(app).get("/healthz");
-    expect(response.statusCode).toBe(503);
+    expect(response.statusCode).toBe(200);
   });
 });
 
