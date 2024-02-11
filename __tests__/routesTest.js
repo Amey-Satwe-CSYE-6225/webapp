bdd6fa8fc2ab5262d7ad8fe09adf3ee7872af3b4
Amey Satwe <amey10051999@gmail.com>
2024-02-11 15:40:47 -0500

    added other test. Working on perfecting integration testing

--- __tests__/routesTest.js
+++ __tests__/routesTest.js
@@ -9,6 +9,13 @@ const payload = {
   username: "amey.satwe@test.com",
 };
 
+const putPayload = {
+  first_name: "AmeyAfterPut",
+  last_name: "satwe",
+  password: "amey",
+  username: "amey.satwe@test.com",
+};
+
 describe("GET /healthz", () => {
   it("responds with 200 if db connects", async () => {
     const response = await request(app).get("/healthz");
@@ -28,6 +35,27 @@ describe("POST /healthz", () => {
       .get("/v1/user/self")
       .set("Authorization", `Basic ${btoa(stringToEncode)}`);
     expect(getResponse.status).toBe(200);
+    // console.log(getResponse.body);
     expect(getResponse.body).toMatchObject(responseBody);
+  });
+});
+
+describe("PUT /healthz", () => {
+  it("Get updated schema from the request", async () => {
+    const stringToEncode = payload.username + ":" + payload.password;
+    const putResponse = await request(app)
+      .put("/v1/user/self")
+      .send(putPayload)
+      .set("Authorization", `Basic ${btoa(stringToEncode)}`)
+      .expect(204);
+    // const stringToEncode = payload.username + ":" + payload.password;
+    const getResponse = await request(app)
+      .get("/v1/user/self")
+      .set("Authorization", `Basic ${btoa(stringToEncode)}`);
+    expect(getResponse.status).toBe(200);
+    console.log("get body from put" + getResponse.body);
+    delete putPayload.password;
+    console.log(getResponse.body);
+    expect(getResponse.body).toMatchObject(putPayload);
   });
 });
