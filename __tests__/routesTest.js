cbb92397e6d52689b1e3c89f09f1df8b5d0539ac
Amey Satwe <amey10051999@gmail.com>
2024-02-11 12:41:35 -0500

    Adding integration testing. and also trying to configure mysql to run on the github runner

--- __tests__/routesTest.js
+++ __tests__/routesTest.js
@@ -2,9 +2,34 @@
 const request = require("supertest");
 const app = require("../main");
 
+const payload = {
+  first_name: "amey",
+  last_name: "satwe",
+  password: "amey",
+  username: "amey.satwe@test.com",
+};
+
 describe("GET /healthz", () => {
   it("responds with 200 if db connects", async () => {
     const response = await request(app).get("/healthz");
     expect(response.statusCode).toBe(503);
+  });
+});
+
+describe("POST /healthz", () => {
+  it("responds with schema if user is created successfully", async () => {
+    const postResponse = await request(app)
+      .post("/v1/user")
+      .send(payload)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
+    expect(postResponse.status).toBe(200);
+    const responseBody = postResponse.body;
+    const stringToEncode = payload.username + ":" + payload.password;
+    const getResponse = await request(app)
+      .get("/v1/user/self")
+      .set("Authorization", `Basic ${btoa(stringToEncode)}`);
+    expect(getResponse.status).toBe(200);
+    expect(getResponse.body).toMatchObject(responseBody);
   });
 });
