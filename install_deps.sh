3f12781af55728c656b37b1c8d4dc44dd3da7f26
Amey Satwe <amey10051999@gmail.com>
2024-02-19 00:39:45 -0500

    Working on packer locally. time to move everything to github

---
+++ install_deps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo groupadd csye6225
+sudo useradd -m -s /usr/sbin/nologin -g csye6225 csye6225
+
+
+sudo cp /tmp/webapp.zip /opt/
+cd /opt/
+
+sudo yum makecache
+sudo yum install unzip -y
+sudo unzip webapp.zip -d /opt/webapp/
+ls -ltr 
+
+sudo chown -R csye6225:csye6225 ./webapp/
+cd webapp
+ls -ltr
+
+sudo dnf install mysql-server -y
+
+# To start the server
+sudo systemctl start mysqld.service
+
+sudo dnf module install nodejs:18/common -y
+
+node -v
+
+npm i
