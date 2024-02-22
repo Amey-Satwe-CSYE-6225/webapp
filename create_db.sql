c4be58b892365caa850154b12cd340fe2bb781e8
Amey Satwe <amey10051999@gmail.com>
2024-02-21 19:53:25 -0500

    db.sql file is now a secret. Maintaining credentials is easier

--- create_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS Assignment_1;
-
-CREATE USER 'sammy'@'localhost' IDENTIFIED WITH mysql_native_password BY '7507';
-
-GRANT ALL ON Assignment_1.* TO 'sammy'@'localhost' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
