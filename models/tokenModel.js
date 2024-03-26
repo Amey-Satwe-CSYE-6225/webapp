f191c168bfaeb5f2604e8a0db4fbf9588316d722
Amey Satwe <amey10051999@gmail.com>
2024-03-26 19:23:55 -0400

    Initial work for assignment 7. Verify user endpoint added. Working on more verification. Token generation logic added. Messages are sent in pub/sub

---
+++ models/tokenModel.js
@@ -0,0 +1,32 @@
+const sequelize = require("./sequelize.js");
+const DataTypes = require("sequelize");
+
+const Tokens = sequelize.define(
+  "Tokens",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    token: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    expiry: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Tokens;
