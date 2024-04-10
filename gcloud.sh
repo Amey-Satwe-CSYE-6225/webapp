4784ea8acbfdfa00d2deaebfb35758ec90702f73
Amey Satwe <amey10051999@gmail.com>
2024-04-10 09:53:59 -0400

    update script file to echo stuff

--- gcloud.sh
+++ gcloud.sh
@@ -6,7 +6,10 @@ DATABASE=$(cat ./secret.json | python3 -c "import sys, json; print(json.load(sys
 UNAME==$(cat ./secret.json | python3 -c "import sys, json; print(json.load(sys.stdin)['UNAME'])")
 PASSWORD==$(cat ./secret.json | python3 -c "import sys, json; print(json.load(sys.stdin)['PASSWORD'])")
 HOST==$(cat ./secret.json | python3 -c "import sys, json; print(json.load(sys.stdin)['HOST'])")
-
+echo $DATABASE
+echo $UNAME
+echo $PASSWORD
+echo $HOST
 
 gcloud compute instance-templates create webapp-from-git --instance-template-region="us-east1" --tags="webapp" --machine-type="n2-standard-2" --region="us-east1" --network-interface=network="cloud-demo-vpc",subnet="webapp" --service-account=csye-demo-service-account@csye-6225-demo-413900.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=load-balanced-backend,csye-6225 --maintenance-policy="MIGRATE" --image-project="csye-6225-demo-413900" --image=$IMAGE_NAME --boot-disk-type="pd-standard" --boot-disk-kms-key="projects/csye-6225-demo-413900/locations/us-east1/keyRings/keyringexamplemLzA/cryptoKeys/cryptokeyexamplemLzA"
 
